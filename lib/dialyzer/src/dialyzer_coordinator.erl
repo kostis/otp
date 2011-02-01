ff7c3b4c91d59691748f645ac429cda2f10ba815
Kostis Sagonas <kostis@cs.ntua.gr>
2011-02-01 11:14:11 +0200

    Various cleanups of Ypatia's code

--- lib/dialyzer/src/dialyzer_coordinator.erl
+++ lib/dialyzer/src/dialyzer_coordinator.erl
@@ -2,7 +2,7 @@
 %%----------------------------------------------------------------------
 %% %CopyrightBegin%
 %%
-%% Copyright Ericsson AB 2010. All Rights Reserved.
+%% Copyright Ericsson AB 2011. All Rights Reserved.
 %%
 %% The contents of this file are subject to the Erlang Public License,
 %% Version 1.1, (the "License"); you may not use this file except in
@@ -37,14 +37,14 @@
 %%----------------------------------------------------------------------
 
 -spec coordinate_find(dialyzer_callgraph:callgraph(), pid()) ->
-			 {fixpoint} | {not_fixpoint, ordset(label())}.
-			 
+	 'fixpoint' | {'not_fixpoint', ordset(label())}.
+
 coordinate_find(Callgraph, Parent) ->
   Fast = dialyzer_callgraph:get_fast_plt(Callgraph),
   Digraph = dialyzer_callgraph:get_digraph2(Callgraph),
   CondensedDigraph = digraph_utils:condensation(Digraph),
   ServerPid = self(),
-  Edges = [digraph:edge(CondensedDigraph, E) 
+  Edges = [digraph:edge(CondensedDigraph, E)
 	   || E <- digraph:edges(CondensedDigraph)],
   SelfEdges = [E || {E, V, V, _} <- Edges],
   true = digraph:del_edges(CondensedDigraph, SelfEdges),
@@ -57,7 +57,7 @@ coordinate_find(Callgraph, Parent) ->
   find_server([], CondensedDigraph, Fast, CoordinatorPid).
 
 -spec coordinate_refine(digraph(), dialyzer_callgraph:callgraph(), pid()) ->
-			 {fixpoint} | {not_fixpoint, ordset(label())}.
+	 'fixpoint' | {'not_fixpoint', ordset(label())}.
 
 coordinate_refine(ModuleDigraph, Callgraph, Parent) ->
   ServerPid = self(),
@@ -74,27 +74,27 @@ coordinate_refine(ModuleDigraph, Callgraph, Parent) ->
 
 coordinate_warnings(Modules, State, BehavioursChk, CWarns) ->
   ServerPid = self(),
-  [spawn(
-     fun() ->
-	 dialyzer_succ_typings:get_warnings_from_module_parallel(M, State,
-								 BehavioursChk, 
-								 ServerPid)
-     end) || M <- Modules],
+  lists:foreach(fun (M) ->
+		    spawn(fun () ->
+			      dialyzer_succ_typings:get_warnings_from_module_parallel(M, State, BehavioursChk, ServerPid)
+			  end)
+		end, Modules),
   Warns = [receive {warnings, Warnings} -> Warnings end || _M <- Modules],
-  lists:flatten(CWarns++Warns).
+  lists:flatten(CWarns ++ Warns).
 
 find_server(NotFixpoint, CondensedDigraph, Fast, CoordinatorPid) ->
   receive
     {results, SCC, SuccTypes, PltContracts, NewNotFixPoint1} ->
       NewNotFixpoint2 = ordsets:union(NewNotFixPoint1, NotFixpoint),
       Parents = get_parents(SCC, CondensedDigraph),
-      dialyzer_succ_typings:insert_into_plt(SuccTypes, undefined, true),
+      dialyzer_succ_typings:insert_into_plt_parallel(SuccTypes),
       true = dialyzer_plt:insert_contract_list(undefined, PltContracts, true),
       case Parents of
 	[] ->
 	  ok;
 	_->
-	  CoordinatorPid ! Parents
+	  CoordinatorPid ! Parents,
+	  ok
       end,
       true = digraph:del_vertex(CondensedDigraph, SCC),
       case digraph:no_vertices(CondensedDigraph) =:= 0 of
@@ -102,8 +102,8 @@ find_server(NotFixpoint, CondensedDigraph, Fast, CoordinatorPid) ->
 	  CoordinatorPid ! stop,
 	  case dialyzer_succ_typings:check_fixpoint(NewNotFixpoint2, undefined, 
 						    Fast, true) of
-	    false -> {not_fixpoint, NewNotFixpoint2};
-	    true ->  {fixpoint}
+	    true -> fixpoint;
+	    false -> {not_fixpoint, NewNotFixpoint2}
 	  end;
 	false ->
 	  find_server(NewNotFixpoint2, CondensedDigraph, Fast, CoordinatorPid)
@@ -115,47 +115,59 @@ refine_server(NotFixpoint, ModuleDigraph, CoordinatorPid) ->
     {true, M, FixpointFromScc} ->
       ok;
     {false, M, DictFixpoint, FixpointFromScc} ->
-      dialyzer_succ_typings:insert_into_plt(DictFixpoint, undefined, true)
-  end,     
+      dialyzer_succ_typings:insert_into_plt_parallel(DictFixpoint)
+  end,
   NewFixpoint = ordsets:union(NotFixpoint, FixpointFromScc),
   Parents = get_parents(M, ModuleDigraph),
   case Parents of
     [] ->
       ok;
     _->
-      CoordinatorPid ! Parents
+      CoordinatorPid ! Parents,
+      ok
   end,
   true = digraph:del_vertex(ModuleDigraph, M),
   case digraph:no_vertices(ModuleDigraph) =:= 0 of
     true ->
       CoordinatorPid ! stop,
       case NewFixpoint =:= [] of
-	false -> {not_fixpoint, NewFixpoint};
-	true ->  {fixpoint}
+	true -> fixpoint;
+	false -> {not_fixpoint, NewFixpoint}
       end;
     false ->
       refine_server(NewFixpoint, ModuleDigraph, CoordinatorPid)		
-  end.    
+  end.
+
+-spec find_succ_typings_in_parallel(pid(), digraph(),
+				    [dialyzer_callgraph:scc()],
+				    boolean(), pid()) -> no_return().
 
 find_succ_typings_in_parallel(Parent, CondensedDigraph, ReadyList, 
 			      Fast, ServerPid) ->
   case digraph:no_vertices(CondensedDigraph) =:= 0 of
     true ->
-      %%    io:format ( "\nTypesig done"),
+      %% io:format ( "\nTypesig done"),
       receive stop ->
 	  exit(normal)
       end;
     false ->
-      _Pids = [spawn(
-		 fun() ->
-		     dialyzer_succ_typings:analyze_scc_parallel(SCC, ServerPid, 
-								Fast)
-		 end) || SCC <- ReadyList],
-      [update_info(SCC, Parent, "Typesig analysis of SCC:") || SCC <- ReadyList],
+      lists:foreach(fun (SCC) ->
+			spawn(fun () ->
+				  dialyzer_succ_typings:analyze_scc_parallel(SCC, ServerPid, Fast)
+			      end)
+		    end, ReadyList),
+      lists:foreach(fun (SCC) ->
+			update_info(SCC, Parent, "Typesig analysis of SCC:")
+		    end, ReadyList),
       receive NewReadyList -> ok end,
       find_succ_typings_in_parallel(Parent, CondensedDigraph, NewReadyList,
-				    Fast, ServerPid)	    
+				    Fast, ServerPid)
   end.
+
+-spec refine_succ_typings_in_parallel(pid(), digraph(),
+				      dialyzer_callgraph:callgraph(),
+				      [dialyzer_callgraph:scc()],
+				      pid()) -> no_return().
 
 refine_succ_typings_in_parallel(Parent, ModuleDigraph, Callgraph, 
 				ReadyList, ServerPid) ->
@@ -165,19 +177,19 @@ refine_succ_typings_in_parallel(Parent, ModuleDigraph, Callgraph,
 	  exit(normal)
       end;
     false ->
-       _Pids = [spawn(fun() ->
-		 case SCC of 
-		   [M] -> 
-		     dialyzer_succ_typings:refine_one_module_parallel(M, 
-								      Callgraph, 
-								      ServerPid);
-		   [_|_] -> 
-		     dialyzer_succ_typings:refine_one_scc_parallel(SCC, 
-								   Callgraph, 
-								   ServerPid)
-		 end 
-	       end) || SCC <- ReadyList],
-      [update_info(SCC, Parent, "Dataflow of one SCC:") || SCC <- ReadyList],
+      lists:foreach(fun (SCC) ->
+			spawn(fun () ->
+				  case SCC of 
+				    [M] -> 
+				      dialyzer_succ_typings:refine_one_module_parallel(M, Callgraph, ServerPid);
+				    [_|_] -> 
+				      dialyzer_succ_typings:refine_one_scc_parallel(SCC, Callgraph, ServerPid)
+				  end 
+			      end)
+		    end, ReadyList),
+      lists:foreach(fun (SCC) ->
+			update_info(SCC, Parent, "Dataflow of one SCC:")
+		    end, ReadyList),
       receive NewReadyList -> ok end,
       refine_succ_typings_in_parallel(Parent, ModuleDigraph, Callgraph, 
 				      NewReadyList, ServerPid)	    
@@ -185,30 +197,30 @@ refine_succ_typings_in_parallel(Parent, ModuleDigraph, Callgraph,
 
 update_info(SCC, Parent, Mode) ->
   Msg = io_lib:format("~s ~w\n", [Mode,  [MFA || {_M, _F, _A} = MFA <- SCC]]),
-  %%  io:format("MESSAGE : ~p \n",[Msg]),
+  %% io:format("MESSAGE : ~p \n",[Msg]),
   %% ?debug("~s", [Msg]),
   Parent ! {self(), log, lists:flatten(Msg)}.
 
 get_parents(Node, Digraph) ->
-  Vertices = digraph:in_neighbours(Digraph , Node),
-  [V || V <- Vertices, digraph:out_degree(Digraph , V) =:= 1].
+  Vertices = digraph:in_neighbours(Digraph, Node),
+  [V || V <- Vertices, digraph:out_degree(Digraph, V) =:= 1].
 
 -spec make_global_st(dialyzer_callgraph:callgraph(), 
 		     dialyzer_codeserver:codeserver(),
 		     dialyzer_plt:plt(), boolean()) -> true.
 
 make_global_st(Callgraph, Codeserver, Plt, Parallel) ->
-  case Parallel of 
+  case Parallel of
     true ->
-      ?Codeserver = ets:new(?Codeserver,[protected, named_table]),
+      ?Codeserver = ets:new(?Codeserver, [protected, named_table]),
       true = ets:insert(?Codeserver, 
 			{next_core_label,  
 			 dialyzer_codeserver:get_next_core_label(Codeserver)}),
-      ?Cs_Records = ets:new(?Cs_Records,[protected, named_table]),
+      ?Cs_Records = ets:new(?Cs_Records, [protected, named_table]),
       true = ets:insert(?Cs_Records, 
 			dict:to_list(
 			  dialyzer_codeserver:get_records(Codeserver))),
-      ?Cs_Contracts = ets:new(?Cs_Contracts,[protected, named_table]),
+      ?Cs_Contracts = ets:new(?Cs_Contracts, [protected, named_table]),
       true = ets:insert(?Cs_Contracts, 
 			dict:to_list(
 			  dialyzer_codeserver:get_contracts(Codeserver))),
@@ -216,32 +228,32 @@ make_global_st(Callgraph, Codeserver, Plt, Parallel) ->
       true = ets:insert(?Cs_Exported_Types, 
 			{exp_types, 
 			 dialyzer_codeserver:get_exported_types(Codeserver)}),
-      ?Callgraph = ets:new(?Callgraph,[protected, named_table]),
+      ?Callgraph = ets:new(?Callgraph, [protected, named_table]),
       true = ets:insert(?Callgraph, 
 			[{selfrec, dialyzer_callgraph:get_self_rec(Callgraph)},
 			 {esc, dialyzer_callgraph:get_escaping(Callgraph)}]),
-      ?Cg_RecVarMap = ets:new(?Cg_RecVarMap,[protected, named_table]),
+      ?Cg_RecVarMap = ets:new(?Cg_RecVarMap, [protected, named_table]),
       true = ets:insert(?Cg_RecVarMap, 
 			dict:to_list(
 			  dialyzer_callgraph:get_rec_var_map(Callgraph))),
-      ?Cg_RevNameMap = ets:new(?Cg_RevNameMap,[protected, named_table]),
+      ?Cg_RevNameMap = ets:new(?Cg_RevNameMap, [protected, named_table]),
       true = ets:insert(?Cg_RevNameMap, 
 			dict:to_list(
 			  dialyzer_callgraph:get_rev_name_map(Callgraph))),
-      ?Cg_NameMap = ets:new(?Cg_NameMap,[protected, named_table]),
+      ?Cg_NameMap = ets:new(?Cg_NameMap, [protected, named_table]),
       true = ets:insert(?Cg_NameMap, 
 			dict:to_list(
 			  dialyzer_callgraph:get_name_map(Callgraph))),
-      ?Cg_Calls = ets:new(?Cg_Calls,[protected, named_table]),
+      ?Cg_Calls = ets:new(?Cg_Calls, [protected, named_table]),
       true = ets:insert(?Cg_Calls, 
 			dict:to_list(dialyzer_callgraph:get_calls(Callgraph))),
-      ?Plt_Info = ets:new(?Plt_Info,[public, named_table]), 
-      ?OldPlt_Info = ets:new(?OldPlt_Info,[protected, named_table]), 
+      ?Plt_Info = ets:new(?Plt_Info, [public, named_table]), 
+      ?OldPlt_Info = ets:new(?OldPlt_Info, [protected, named_table]), 
       Infos = dict:to_list(dialyzer_plt:get_info(Plt)),
       true = ets:insert(?Plt_Info, Infos),
       true = ets:insert(?OldPlt_Info, Infos),
-      ?Plt_Contracts = ets:new(?Plt_Contracts,[protected, named_table]), 
-      ?OldPlt_Contracts = ets:new(?OldPlt_Contracts,[protected, named_table]), 
+      ?Plt_Contracts = ets:new(?Plt_Contracts, [protected, named_table]), 
+      ?OldPlt_Contracts = ets:new(?OldPlt_Contracts, [protected, named_table]), 
       PltContracts = dict:to_list(dialyzer_plt:get_contracts(Plt)),
       true = ets:insert(?Plt_Contracts, PltContracts),
       true = ets:insert(?OldPlt_Contracts, PltContracts);
@@ -250,8 +262,8 @@ make_global_st(Callgraph, Codeserver, Plt, Parallel) ->
   end.
 
 -spec delete_global_st(dialyzer_plt:plt()) -> dialyzer_plt:plt().
-			  
-delete_global_st(Plt)->
+		  
+delete_global_st(Plt) ->
   Contracts = ets:tab2list(?Plt_Contracts),
   Plt1 = dialyzer_plt:set_contracts(Plt, dict:from_list(Contracts)),
   Plt_Info = ets:tab2list(?Plt_Info),
@@ -272,4 +284,3 @@ delete_global_st(Plt)->
   ets:delete(?Mfa_Codeserver),
   ets:delete(?Mod_Codeserver),
   Plt2.
-
